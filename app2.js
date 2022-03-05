1e311952c0b45e93367d3e63534889339c0c1e9c
KIRTI MANDAL <99232521+Kirtimandal08@users.noreply.github.com>
2022-03-04 22:10:30 -0500

    Add files via upload

---
+++ app2.js
@@ -0,0 +1,14 @@
+const btn = document.querySelectorAll('button');
+
+const randomColor = () =>{
+    const r = Math.floor(Math.random()*255);
+    const g = Math.floor(Math.random()*255);
+    const b = Math.floor(Math.random()*255);
+    return `rgb(${r}, ${g}, ${b})`;
+}
+
+for (let button of btn) {
+    button.addEventListener('click', function(){
+        button.style.backgroundColor = randomColor();
+    })
+}
